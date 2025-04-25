19a39ac12eef00faa75156c6df4b211b63228106
Suganya <gs.suganya16@gmail.com>
2025-04-25 16:11:24 +1000

    Fix serverless code

--- src/api/login.js
+++ src/api/login.js
@@ -1,9 +1,9 @@
 export default async function handler(req,res){
     if(req.method == 'POST'){
         const{username,password} = req.body;
-        console.log('Login Successful', username);
+        console.log('Login Successful', {username});
         return res.status(200).json({success:true,message:username});
     }
     res.setHeader('Allow',[POST]);
-    res.status(405).end(`Method ${req.method} not allowed` );
+    res.status(405).json({error:`Method ${req.method} not allowed`} );
 }
